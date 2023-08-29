24308eea11471b123713bd9de528184c5ed92286
dziwa <dziwa@ANDREW_LAPTOP>
2023-08-29 13:53:45 -0500

    Add project files.

---
+++ GH1.cpp
@@ -0,0 +1,11 @@
+// GH1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This is my first time using GitHub." << endl;
+}
+
